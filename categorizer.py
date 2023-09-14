324f76e44f144cd62db5569a077702479809611f
Svulster <140300305+Svulster@user.noreply.github.com>
2023-09-14 23:43:57 +0200

    added categorizer and update_category functions

---
+++ categorizer.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+filepath = "database/categories.xlsx"
+sheet = 0
+
+categories = pd.read_excel(filepath, sheet).drop(columns="Unnamed: 0")
+
+def categorize(transaction_name): #pandas version
+    if categories["Hyra"].eq(transaction_name).any():
+        return "Hyra"
+    elif categories["Lön"].eq(transaction_name).any():
+        return "Lön"
+    elif categories["Bidrag"].eq(transaction_name).any():
+        return "Bidrag"
+    elif categories["Sparande"].eq(transaction_name).any():
+        return "Sparande"
+    elif categories["Försäkring"].eq(transaction_name).any():
+        return "Försäkring"
+    elif categories["Överföringar"].eq(transaction_name).any():
+        return "Intern_överföring"
+    elif categories["Mat"].eq(transaction_name).any():
+        return "Mat"
+    elif categories["Mat ute"].eq(transaction_name).any():
+        return "Mat ute"
+    elif categories["Hem"].eq(transaction_name).any():
+        return "Hem"
+    elif categories["Transport"].eq(transaction_name).any():
+        return "Transport"
+    elif categories["Abonnemang"].eq(transaction_name).any():
+        return "Abonnemang"
+    elif categories["Kläder"].eq(transaction_name).any():
+        return "Kläder"
+    elif categories["Barn"].eq(transaction_name).any():
+        return "Barn"
+    else:
+        return "Okategoriserad"
+    
+def update_category(transaction_name):
+    print(transaction_name)
+    cat = input("Ange kategori: ")
+
+    for i in range(len(categories[cat])):
+        if pd.isna(categories.at[i,cat]):
+            categories.at[i, cat] = transaction_name
+            break
+    
+    categories.to_excel(filepath)
